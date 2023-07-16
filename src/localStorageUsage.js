bb186c7f6c60da49f8d22ce9a1b12150876eec89
CodeGPTHub <139706396+CodeGPTHub@users.noreply.github.com>
2023-07-16 20:58:00 +0300

    Create localStorageUsage.js

---
+++ src/localStorageUsage.js
@@ -0,0 +1,8 @@
+// Storing data in local storage
+localStorage.setItem("username", "JohnDoe");
+
+// Retrieving data from local storage
+const username = localStorage.getItem("username");
+
+// Removing data from local storage
+localStorage.removeItem("username");
