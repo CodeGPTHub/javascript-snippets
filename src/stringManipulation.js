e221aea4273aa1d771f231998091b3b059f2f008
CodeGPTHub <139706396+CodeGPTHub@users.noreply.github.com>
2023-07-16 20:57:33 +0300

    Create stringManipulation.js

---
+++ src/stringManipulation.js
@@ -0,0 +1,14 @@
+const message = "Hello, World!";
+
+// String length
+const length = message.length;
+
+// String uppercase/lowercase
+const uppercaseMessage = message.toUpperCase();
+const lowercaseMessage = message.toLowerCase();
+
+// String replace
+const replacedMessage = message.replace("World", "JavaScript");
+
+// String splitting
+const words = message.split(" ");
